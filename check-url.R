72c9f3288252943270de2864db137d6cd65e7376
Nan Xiao <me@nanx.me>
2023-01-15 00:41:10 -0500

    Remove file name assumptions for URL checking with flatten copy logic

--- check-url.R
+++ check-url.R
@@ -1,47 +1,63 @@
+#' Flatten copy
+#'
+#' @param from Source directory path
+#' @param to Destination directory path
+#'
+#' @details Copy all `.Rmd` and `.md` files from source to destination,
+#' rename the `.md` files with an additional `.Rmd` extension, and get a
+#' flat destination directory structure with path-preserving file names.
+flatten_copy <- function(from, to) {
+  rmd <- list.files(from, pattern = "\\.Rmd$", recursive = TRUE, full.names = TRUE)
+  md <- list.files(from, pattern = "\\.md", recursive = TRUE, full.names = TRUE)
+
+  src <- c(rmd, md)
+  dst <- c(rmd, paste0(md, ".Rmd"))
+
+  # Remove starting `./` (if any)
+  dst <- gsub("^\\./", replacement = "", x = dst)
+  # Replace the forward slash in path with Unicode big solidus
+  dst <- gsub("/", replacement = "\u29F8", x = dst)
+
+  file.copy(src, to = file.path(to, dst))
+
+  invisible(to)
+}
+
 #' Check URLs in a bookdown project
 #'
 #' @param input Path to the bookdown project directory.
 #'
 #' @return URL checking results from `urlchecker::url_check()`
-#'
-#' @details This assumes there are no `.Rmd` and `.md` files with
-#' duplicated names in the bookdown project.
+#' for all `.Rmd` and `.md` files in the project.
 #'
 #' @note The `tools::pkgVignettes()$docs` call in urlchecker
-#' requires two things (VignetteBuilder and VignetteEngine)
+#' requires two things (`VignetteBuilder` and `VignetteEngine`)
 #' to recognize `.Rmd` files as package vignettes.
 check_url <- function(input = ".") {
-  pkg_path <- tempfile()
-  dir.create(pkg_path)
+  # Create a source package directory
+  pkg <- tempfile()
+  dir.create(pkg)
 
-  # Create a DESCRIPTION file
-  write("VignetteBuilder: knitr", file = file.path(pkg_path, "DESCRIPTION"))
+  # Create a minimal DESCRIPTION file
+  write("VignetteBuilder: knitr", file = file.path(pkg, "DESCRIPTION"))
 
-  # Copy all .Rmd and .md files
-  rmd_path <- file.path(pkg_path, "vignettes")
-  dir.create(rmd_path)
-  file.copy(
-    list.files(path = input, pattern = "\\.Rmd$|\\.md$", recursive = TRUE),
-    to = rmd_path,
-    overwrite = TRUE
-  )
-  # Rename .md to .Rmd
-  md <- list.files(rmd_path, pattern = "\\.md$", full.names = TRUE)
-  if (length(md)) file.rename(md, to = paste0(tools::file_path_sans_ext(md), ".Rmd"))
+  # Flatten copy all .Rmd and .md files
+  vig <- file.path(pkg, "vignettes")
+  dir.create(vig)
+  flatten_copy(input, vig)
 
-  # Make them look like vignettes
+  # Make the .Rmd files look like vignettes
   lapply(
-    list.files(rmd_path, full.names = TRUE),
+    list.files(vig, full.names = TRUE),
     function(x) {
       write(
         "---\nvignette: >\n  %\\VignetteEngine{knitr::rmarkdown}\n---",
-        file = x,
-        append = TRUE
+        file = x, append = TRUE
       )
     }
   )
 
-  urlchecker::url_check(pkg_path)
+  urlchecker::url_check(pkg)
 }
 
 check_url()
