b86f2dff980140475f8b8ab397f65231419fd734
Nan Xiao <me@nanx.me>
2024-02-21 19:39:21 -0500

    Customize data frame and tibble printing methods

--- _common.R
+++ _common.R
@@ -13,3 +13,25 @@ rtf2pdf <- function(input) {
   z <- paste(x, y, sep = " && ")
   if (Sys.getenv("GITHUB_ACTIONS") != "") system(z) else invisible(NULL)
 }
+
+# Customize data frame and tibble printing methods.
+# See <https://github.com/elong0527/r4csr/issues/115> for details.
+knit_print.data.frame <- function(x, ...) {
+  paste(capture.output(base::print.data.frame(head(x, 4))), collapse = "\n")
+}
+
+registerS3method(
+  "knit_print", "data.frame", knit_print.data.frame,
+  envir = asNamespace("knitr")
+)
+
+options(pillar.advice = FALSE)
+
+knit_print.tbl_df <- function(x, ...) {
+  paste(capture.output(print(x, n = 4)), collapse = "\n")
+}
+
+registerS3method(
+  "knit_print", "tbl_df", knit_print.tbl_df,
+  envir = asNamespace("knitr")
+)
